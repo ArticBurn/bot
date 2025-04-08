b4644059fc429c02f71c2f1be004a19e828b0f72
Artic Burn <ivanandika94@gmail.com>
2025-04-08 09:35:51 +0700

    Update main.py
    refactoring code

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ import os
 
 from keep_alive import keep_alive
 
-
-# Masukkan token bot kamu
 TOKEN = os.getenv("DISCORD_TOKEN")
 URL = "https://cdn2.arkdedicated.com/asa/dynamicconfig.ini"
 
-# Inisialisasi bot
+# initiate
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
 
@@ -21,25 +19,11 @@ async def on_ready():
 
 # Rich Presence (Custom Status)
     activity = discord.Game(
-        # type=discord.ActivityType.,  # Bisa juga: playing, listening, streaming
-        name="Shit with scammed ppl"  # Pesan yang ditampilkan di status
+        name="Shit with scammed ppl" 
     )
     await bot.change_presence(status=discord.Status.online, activity=activity)
 
-@bot.command()
-async def config(ctx):
-    """Fetching data from arkdedicated and show to guild"""
-    try:
-        response = requests.get(URL)
-        if response.status_code == 200:
-            data = response.text[:2000]  # Batasi 2000 karakter (batas Discord)
-            await ctx.send(f"```ini\n{data}\n```")
-        else:
-            await ctx.send("⚠️ Failed for fetching the data.")
-    except Exception as e:
-        await ctx.send(f"❌ Error: {e}")
-
-# Slash command /rate
+# command /raw
 @bot.tree.command(name="raw", description="Fetching data from arkdedicated")
 async def rate(interaction: discord.Interaction):
     """Fetching data from arkdedicated and show to guild"""
@@ -53,6 +37,6 @@ async def rate(interaction: discord.Interaction):
     except Exception as e:
         await interaction.response.send_message(f"❌ Error: {e}")
 
-# Jalankan bot
+# run_bot
 keep_alive()
 bot.run(TOKEN)
